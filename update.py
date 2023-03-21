9e6e6600d0d115ac4558158e2c2f983d322df6ed
MR X MIRROR <127676677+MR-X-MIRROR-BOTZ@users.noreply.github.com>
2023-03-21 15:34:38 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/arshsisodiya/helios-mirror"
+    UPSTREAM_REPO = "https://github.com/MR-X-MIRROR-BOTZ/MR-HELIOS-MIRROR"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
