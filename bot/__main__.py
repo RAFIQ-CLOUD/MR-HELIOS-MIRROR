b46babbe4ca92a4539a89cb45509300d385501ef
RAFIQ-CLOUD <124174722+RAFIQ-CLOUD@users.noreply.github.com>
2023-03-21 17:29:14 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -6,7 +6,7 @@ from time import time
 from sys import executable
 from telegram.ext import CommandHandler
 
-from bot import bot, dispatcher, updater, botStartTime, IGNORE_PENDING_REQUESTS, LOGGER, Interval, INCOMPLETE_TASK_NOTIFIER, DB_URI, app, main_loop, app_session, USER_SESSION_STRING,\
+from bot import bot, dispatcher, updater, botStartTime, TIMEZONE, IGNORE_PENDING_REQUESTS, LOGGER, Interval, INCOMPLETE_TASK_NOTIFIER, DB_URI, app, main_loop, app_session, USER_SESSION_STRING,\
                 OWNER_ID, SUDO_USERS, START_BTN1_NAME, START_BTN1_URL, START_BTN2_NAME, START_BTN2_URL
 from .helper.ext_utils.fs_utils import start_cleanup, clean_all, exit_clean_up
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
